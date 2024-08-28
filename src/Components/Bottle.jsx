d1281ab389cad5aa5f75ad6f5ee51503c4724633
Mirazovi <mdmirazhossainovi@gmail.com>
2024-08-28 17:24:37 +0600

    last commit

--- src/Components/Bottle.jsx
+++ src/Components/Bottle.jsx
@@ -4,7 +4,7 @@ const Bottle = ({bottle,handleAdd}) => {
     return (
         <div className="border-2 border-red-700 rounded-xl p-6 text-center">
             <h1 className="text-2xl font-bold">Title :{name}</h1>
-            <img className="py-2 w-full h-[300px]" src={img} alt="" />
+            <img className="py-2 w-full h-[250px]" src={img} alt="" />
             <p className="text-xl">Price :{price}$</p>
             <p className="text-xl py-2">Stock :{stock} pcs</p>
             <button onClick={()=> handleAdd(bottle)} className="text-xl text-white py-2 px-4 rounded-xl w-full bg-violet-800">Add Now</button>
